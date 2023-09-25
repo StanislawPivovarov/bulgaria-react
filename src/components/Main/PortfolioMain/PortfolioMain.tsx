93014e8198503cf43916382eedd5aaa522815218
Stas <photovarov@bk.ru>
2023-09-25 11:12:02 +0300

    Add fix

--- src/components/Main/PortfolioMain/PortfolioMain.tsx
+++ src/components/Main/PortfolioMain/PortfolioMain.tsx
@@ -4,6 +4,7 @@ import test from '../../../assets/test.jpeg'
 import { RightOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import Marquee from "react-fast-marquee";
+import PopUp from "../../PopUp";
 
 
 const PortfolioMain = () => {
@@ -11,6 +12,11 @@ const PortfolioMain = () => {
         <div className={style.back}>
             <h2 className={style.header}>Портфолио</h2>
             <Marquee>
+                <Button>
+                <div className={style.image_container}>
+                    <img className={style.image} alt="" src={test} />
+                </div>
+                </Button>
                 <div className={style.image_container}>
                     <img className={style.image} alt="" src={test} />
                 </div>
@@ -38,6 +44,8 @@ const PortfolioMain = () => {
 
             </Marquee>
             <Button type="link" className={style.portfolio_link} href="/portfolio">Смотреть все работы <RightOutlined /></Button>
+
+            <PopUp/>
 
         </div>
     )
---
+++ src/components/PopUp/index.tsx
@@ -0,0 +1,50 @@
+import { Button, Modal } from "antd";
+import React, { useState } from "react";
+import test from '../../assets/popup/56d9edc0508af5327e71c157f85c3570.jpeg'
+import style from './PopUp.module.scss'
+
+const PopUp = () => {
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    const width = window.innerWidth
+    var close
+    if (width > 992){
+        close = true
+    }
+    else (
+        close = false
+    )
+    console.log(close)
+
+    return (
+        <div>
+            
+            <Modal centered className={style.popup} footer={false} open={isModalOpen} onOk={handleOk} closable={close} onCancel={handleCancel}>
+                <img src={test} className={style.image} alt="" />
+                <div className={style.popup_content}>
+                    <h1>Профессиональное создание каталога для TopShues</h1>
+                    <p className={style.description}>Мы представляем полный цикл создания каталогов для обувного магазина. Мы работали тесно с клиентом, выбрали оптимальную бумагу, распечатали каталоги и сделали контроль качества. Мы гарантируем максимальное качество нашей работы и готовы представлять продукцию магазина на высоком уровне.</p>
+                    <div className={style.link}>
+                        <p>Товар:</p>
+                        <Button type="default">Буклеты и брошюры</Button>
+                    </div>
+                </div>
+            </Modal>
+
+        </div>
+    )
+}
+
+export default PopUp;
